5962932379fd138dba68add63f43073e7393b6f8
Devon Bautista <devnb@devnb.net>
2024-09-13 13:34:26 -0600

    Move core request functions from BSSClient to OchamiClient

--- internal/client/client.go
+++ internal/client/client.go
@@ -9,8 +9,10 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path"
 	"time"
 
+	"github.com/synackd/ochami/internal/log"
 	"github.com/synackd/ochami/internal/version"
 )
 
@@ -18,8 +20,9 @@ import (
 // being communicated with.
 type OchamiClient struct {
 	*http.Client
-	BaseURI  *url.URL // Base URL for OpenCHAMI services (e.g. https://foobar.openchami.cluster)
-	BasePath string   // Base path for the service (e.g. /boot/v1 for BSS)
+	BaseURI     *url.URL // Base URL for OpenCHAMI services (e.g. https://foobar.openchami.cluster)
+	BasePath    string   // Base path for the service (e.g. /boot/v1 for BSS)
+	ServiceName string   // Name of service being contacted (e.g. BSS)
 }
 
 var (
@@ -53,14 +56,15 @@ func (oc *OchamiClient) defaultClientInsecure() {
 // https://foobar.openchami.cluster) and basePath is the endpoint prefix that is
 // service-dependent (e.g. for BSS it could be "/boot/v1"). If insecure is true,
 // the client will not verify TLS certificates.
-func NewOchamiClient(baseURI, basePath string, insecure bool) (*OchamiClient, error) {
+func NewOchamiClient(serviceName, baseURI, basePath string, insecure bool) (*OchamiClient, error) {
 	u, err := url.Parse(baseURI)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse URI: %v", err)
 	}
 	oc := &OchamiClient{
-		BaseURI:  u,
-		BasePath: basePath,
+		BaseURI:     u,
+		BasePath:    basePath,
+		ServiceName: serviceName,
 	}
 	if insecure {
 		oc.defaultClientInsecure()
@@ -68,6 +72,75 @@ func NewOchamiClient(baseURI, basePath string, insecure bool) (*OchamiClient, er
 		oc.defaultClient()
 	}
 	return oc, err
+}
+
+// GetURI takes an endpoint and joins it with the OchamiClient's BaseURI and
+// BasePath to form the final URI to be used for a request. If query is
+// specified, it is used as a raw query string and appended onto the URL
+// without URL encoding. query should not contain the initial '?'.
+func (oc *OchamiClient) GetURI(endpoint, query string) (string, error) {
+	uri, err := url.Parse(oc.BaseURI.String())
+	if err != nil {
+		return "", fmt.Errorf("failed to parse base URI %s: %v", oc.BaseURI, err)
+	}
+	uri.Path = path.Join(uri.Path, oc.BasePath, endpoint)
+	if query != "" {
+		uri.RawQuery = query
+	}
+	return uri.String(), err
+}
+
+// MakeOchamiRequest is a wrapper around MakeRequest that calls GetURI to form
+// the final URI to make the request with and pass to MakeRequest.
+func (oc *OchamiClient) MakeOchamiRequest(method, endpoint, query string, headers *HTTPHeaders, body HTTPBody) (*http.Response, HTTPBody, error) {
+	uri, err := oc.GetURI(endpoint, query)
+	if err != nil {
+		if query == "" {
+			return nil, nil, fmt.Errorf("failed to generate URI for endpoint %s: %v", endpoint, err)
+		} else {
+			return nil, nil, fmt.Errorf("failed to generate URI for endpoint %s and query %s: %v", endpoint, query, err)
+		}
+	}
+
+	return oc.MakeRequest(method, uri, headers, body)
+}
+
+// GetData is a wrapper around MakeOchamiRequest that sends a GET request to
+// endpoint, using an optional token and optional headers, and returns the data
+// received in the response along with a nil error. If the HTTP response code is
+// unsuccessful (i.e. not 2XX), then the returned error will contain an
+// UnsuccessfulHTTPError. Otherwise, the error that occurred is returned.
+// query is the raw query string (without the '?') to be added to the URI. It
+// should already be URL-encoded, e.g. generated using url.Values' Encode()
+// function.
+func (oc *OchamiClient) GetData(endpoint, query, token string, headers *HTTPHeaders) (string, error) {
+	if token != "" {
+		if headers == nil {
+			headers = NewHTTPHeaders()
+		}
+		if err := headers.SetAuthorization(token); err != nil {
+			return "", fmt.Errorf("error setting token in HTTP headers: %v", err)
+		}
+	}
+
+	res, resBody, err := oc.MakeOchamiRequest(http.MethodGet, endpoint, query, headers, nil)
+	if err != nil {
+		return "", fmt.Errorf("error making request to %s: %v", oc.ServiceName, err)
+	}
+	if res != nil {
+		statusOK := res.StatusCode >= 200 && res.StatusCode < 300
+		if statusOK {
+			log.Logger.Info().Msgf("Response status: %s %s", res.Proto, res.Status)
+			return string(resBody), nil
+		} else {
+			if len(resBody) > 0 {
+				return "", fmt.Errorf("%w: %s %s: %s", UnsuccessfulHTTPError, res.Proto, res.Status, string(resBody))
+			} else {
+				return "", fmt.Errorf("%w: %s %s", UnsuccessfulHTTPError, res.Proto, res.Status)
+			}
+		}
+	}
+	return "", fmt.Errorf("%s response was empty", oc.ServiceName)
 }
 
 // MakeRequest is a convenience function that, using an OchamiClient as the HTTP
