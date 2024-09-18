d55f78aa1ebe0b0ebb527428f9a4390d2e12f6a5
Devon T. Bautista <devonb@lanl.gov>
2024-09-18 04:32:37 -0600

    Add bss-bootscript

---
+++ cmd/bss-bootscript.go
@@ -0,0 +1,40 @@
+// Copyright © 2024 Triad National Security, LLC. All rights reserved.
+//
+// This program was produced under U.S. Government contract 89233218CNA000001
+// for Los Alamos National Laboratory (LANL), which is operated by Triad
+// National Security, LLC for the U.S. Department of Energy/National Nuclear
+// Security Administration. All rights in the program are reserved by Triad
+// National Security, LLC, and the U.S. Department of Energy/National Nuclear
+// Security Administration. The Government is granted for itself and others
+// acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license
+// in this material to reproduce, prepare derivative works, distribute copies to
+// the public, perform publicly and display publicly, and to permit others to do
+// so.
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/synackd/ochami/internal/log"
+)
+
+// bssBootScriptCmd represents the bootscript command
+var bssBootScriptCmd = &cobra.Command{
+	Use:   "bootscript",
+	Short: "Work with BSS boot scripts",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			err := cmd.Usage()
+			if err != nil {
+				log.Logger.Error().Err(err).Msg("failed to print usage")
+				os.Exit(1)
+			}
+			os.Exit(0)
+		}
+	},
+}
+
+func init() {
+	bssCmd.AddCommand(bssBootScriptCmd)
+}
