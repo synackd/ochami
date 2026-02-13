ac4d3218ae6dfeaef2a8654f996203f59ce8ad21
Devon Bautista <17506592+synackd@users.noreply.github.com>
2026-02-13 16:56:43 -0700

    feat: add 'boot config get' command
    Signed-off-by: Devon Bautista <17506592+synackd@users.noreply.github.com>

--- pkg/client/boot_service/bootconfig.go
+++ pkg/client/boot_service/bootconfig.go
@@ -11,6 +11,29 @@ import (
 	"github.com/OpenCHAMI/ochami/pkg/format"
 )
 
+// GetBootConfig is a wrapper that calls the boot-service client's
+// GetBootConfiguration() function, passing it context and a UID. The output is
+// a []byte containing the entity's boot configuration, formatted as outFormat.
+func (bsc *BootServiceClient) GetBootConfig(token string, outFormat format.DataFormat, uid string) ([]byte, error) {
+	// TODO: boot-service client functions don't support tokens yet.
+	_ = token
+
+	ctx, cancel := context.WithTimeout(context.Background(), bsc.Timeout)
+	defer cancel()
+
+	bcfg, err := bsc.Client.GetBootConfiguration(ctx, uid)
+	if err != nil {
+		return nil, fmt.Errorf("request to get boot configuration for %s failed: %w", uid, err)
+	}
+
+	out, err := format.MarshalData(bcfg, outFormat)
+	if err != nil {
+		return nil, fmt.Errorf("formatting boot configuration for %s failed: %w", uid, err)
+	}
+
+	return out, nil
+}
+
 // ListBootConfigs is a wrapper that calls the boot-service client's
 // GetBootConfigurations() function, passing it context. The output is a []byte
 // containing a list of boot configurations formatted as outFormat.
