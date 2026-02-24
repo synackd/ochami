f04692eeedee47b10a0e1eff064e97bbdceb57f6
Devon Bautista <17506592+synackd@users.noreply.github.com>
2026-02-24 12:55:12 -0700

    feat: add 'boot bmc get' command
    Signed-off-by: Devon Bautista <17506592+synackd@users.noreply.github.com>

--- pkg/client/boot_service/bmc.go
+++ pkg/client/boot_service/bmc.go
@@ -11,6 +11,29 @@ import (
 	"github.com/OpenCHAMI/ochami/pkg/format"
 )
 
+// GetBMC is a wrapper that calls the boot-service client's GetBMC() function,
+// passing it context and a UID. The output is a []byte containing the entity's
+// BMC information, formatted as outFormat.
+func (bsc *BootServiceClient) GetBMC(token string, outFormat format.DataFormat, uid string) ([]byte, error) {
+	// TODO: boot-service client functions don't support tokens yet.
+	_ = token
+
+	ctx, cancel := context.WithTimeout(context.Background(), bsc.Timeout)
+	defer cancel()
+
+	bcfg, err := bsc.Client.GetBMC(ctx, uid)
+	if err != nil {
+		return nil, fmt.Errorf("request to get BMC info for %s failed: %w", uid, err)
+	}
+
+	out, err := format.MarshalData(bcfg, outFormat)
+	if err != nil {
+		return nil, fmt.Errorf("formatting BMC info for %s failed: %w", uid, err)
+	}
+
+	return out, nil
+}
+
 // ListBMCs is a wrapper that calls the boot-service client's GetBMCs()
 // function, passing it context. The output is a []byte containing a list of
 // BMC formatted as outFormat.
